16522afb72a21a483b218fa986daf18eeee3f90d
YuriNem <PsychoactivePie@gmail.com>
2018-03-12 16:53:43 +0300

    Add Commander

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 
-console.log('Work!');
+const commander = require('commander');
 
-/* const commander = require('commander');
-
-commander.version('0.0.4', '-v, --version')
-.arguments('<firstConfig> <secondConfig>')
-.description('Compares two configuration files and shows a difference.')
-.option('-f, --format [type]', 'Output format').parse(process.argv);
-
-if (commander.format) {
-  console.log('Hello');
-} */
+commander
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .version('0.0.5', '-V, --version')
+  .parse(process.argv);
